75c9c63f14f875c27b62c691b6ec610a4563a014
Hirantha9731 <charya.2017321@iit.ac.lk>
2025-03-19 02:12:44 +0530

    Get All Train data and Add train data functions implemented

--- src/app/addrecord/addrecord.component.ts
+++ src/app/addrecord/addrecord.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -8,7 +8,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
-
 
 @Component({
   selector: 'app-addrecord',
@@ -21,78 +20,118 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
     MatNativeDateModule,
     MatButtonModule,
     ReactiveFormsModule,
-     // Required for reactive forms
+    // Required for reactive forms
   ],
   templateUrl: './addrecord.component.html',
   styleUrl: './addrecord.component.scss'
 })
 export class AddTrainRecordComponent {
   form !: FormGroup;
-  actionBtn : string = "Save"
+  actionBtn: string = "Save"
 
-  constructor(private fb: FormBuilder, 
-              private api : ApiService,
-              private dialogRef: MatDialogRef<AddTrainRecordComponent>,
-              @Inject(MAT_DIALOG_DATA) public editData : any) {
+  constructor(private fb: FormBuilder,
+    private api: ApiService,
+    private dialogRef: MatDialogRef<AddTrainRecordComponent>,
+    @Inject(MAT_DIALOG_DATA) public editData: any) {
+
     // Initialize the form
     this.form = this.fb.group({
-      date: ['', Validators.required],
-      trainName: ['', Validators.required],
-      fromDestination: ['', Validators.required],
-      toDestination: ['', Validators.required],
-      scheduledTime: ['', Validators.required],
-      delayTime: ['', Validators.required],
+      Date: ['', Validators.required],
+      TrainName: ['', Validators.required],
+      FromDestination: ['', Validators.required],
+      ToDestination: ['', Validators.required],
+      ScheduledTime: ['', Validators.required],
+      DelayTime: ['', Validators.required],
     });
-    if(this.editData){
+
+    if (this.editData) {
       this.actionBtn = "Update";
-      this.form.controls['date'].setValue(this.editData.date);
-      this.form.controls['trainName'].setValue(this.editData.trainName);
-      this.form.controls['fromDestination'].setValue(this.editData.fromDestination);
-      this.form.controls['toDestination'].setValue(this.editData.toDestination);
-      this.form.controls['scheduledTime'].setValue(this.editData.scheduledTime);
-      this.form.controls['delayTime'].setValue(this.editData.delayTime);
+      this.form.controls['Date'].setValue(this.editData.Date);
+      this.form.controls['TrainName'].setValue(this.editData.TrainName);
+      this.form.controls['FromDestination'].setValue(this.editData.FromDestination);
+      this.form.controls['ToDestination'].setValue(this.editData.ToDestination);
+      this.form.controls['DcheduledTime'].setValue(this.editData.ScheduledTime);
+      this.form.controls['DelayTime'].setValue(this.editData.DelayTime);
     }
   }
 
-  
   // Handle form submission
+  // onSubmit() {
+  //   if(!this.editData){
+  //     if (this.form.valid) {
+  //       this.api.postTrainRecord(this.form.value)
+  //       .subscribe({
+  //         next:(res)=>{
+  //           alert("Train Record Added Successfully!")
+  //           this.form.reset();
+  //           this.dialogRef.close('Recorded');
+  //         },
+  //           error:()=>{
+  //             alert("Error While Adding the Train Record!") 
+  //           }
+  //       })
+  //     } else {
+  //       this.updateRecord()
+  //     }
+  //   }
+  // }
+
   onSubmit() {
-    if(!this.editData){
-      if (this.form.valid) {
-      
-        this.api.postTrainRecord(this.form.value)
-        .subscribe({
-          next:(res)=>{
-            alert("Train Record Added Successfully!")
-            this.form.reset();
-            this.dialogRef.close('Recorded');
-          },
-            error:()=>{
-              alert("Error While Adding the Train Record!") 
-            }
-        })
+    if (this.form.valid) {
+      const formData = this.form.value;
+
+      // Convert 24-hour time to 12-hour format with AM/PM
+      formData.ScheduledTime = this.convertTo12HourFormat(formData.ScheduledTime);
+      formData.DelayTime = this.convertTo12HourFormat(formData.DelayTime);
+
+      if (this.editData) {
+        this.updateRecord();
       } else {
-        this.updateRecord()
+        this.api.postTrainRecord(formData)
+          .subscribe({
+            next: (res) => {
+              alert("Train Record Added Successfully!");
+              this.form.reset();
+              this.dialogRef.close('Recorded');
+            },
+            error: () => {
+              alert("Error While Adding the Train Record!");
+            }
+          });
       }
+    } else {
+      alert("Form is invalid. Please check the fields.");
     }
   }
-// Handle cancel button click
+
+
+  // Handle cancel button click
   onCancel() {
-  console.log('Form cancelled');
-  // You can add logic to close the dialog or navigate away
+    console.log('Form cancelled');
+    // You can add logic to close the dialog or navigate away
   }
 
-  updateRecord(){
-    this.api.putTrainRecord(this.form.value,this.editData.id)
-    .subscribe({
-      next:(res) => {
-        alert("Record Updated Successfully!");
-        this.form.reset();
-        this.dialogRef.close('Updated');
-      },
-      error:()=>{
-        alert("Error While Updating the Record!");
-      }
-    })
+  updateRecord() {
+    this.api.putTrainRecord(this.form.value, this.editData.id)
+      .subscribe({
+        next: (res) => {
+          alert("Record Updated Successfully!");
+          this.form.reset();
+          this.dialogRef.close('Updated');
+        },
+        error: () => {
+          alert("Error While Updating the Record!");
+        }
+      })
+  }
+
+  // Function to convert 24-hour time to 12-hour time with AM/PM
+  convertTo12HourFormat(time: string): string {
+    const [hours, minutes] = time.split(':');
+    const hours12 = parseInt(hours, 10) % 12 || 12;
+    const suffix = parseInt(hours, 10) < 12 ? 'AM' : 'PM';
+    const hours12WithLeadingZero = hours12 < 10 ? `0${hours12}` : `${hours12}`;
+
+    return `${hours12WithLeadingZero}:${minutes} ${suffix}`;
   }
 }
